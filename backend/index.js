5e02c48feeea9937c4a319b041b8f73c0d2a29d0
Sadhika1012 <111134246+Sadhika1012@users.noreply.github.com>
2023-06-20 18:15:27 +0530

    Updated index.js

--- backend/index.js
+++ backend/index.js
@@ -1,12 +1,26 @@
-const express  = require('express');
+const express = require('express');
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 const cors = require('cors');
+const loginsRouter = require('./routes/logins');
+
 dotenv.config();
 
 const app = express();
-app.use(cors()); 
-app.use(express.json())
-app.listen(8080, ()=>{
-    console.log('Backend running and server up and running at 8080!');
-})
+app.use(cors());
+app.use(express.json());
+
+mongoose.connect("mongodb://localhost:27017/CampusKart", {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+  })
+  .then(() => {
+    console.log("MongoDB connection successful");
+  })
+  .catch((err) => console.log(err));
+
+app.use('/api/logins', loginsRouter);
+
+app.listen(8080, () => {
+  console.log('Backend running and server up and running at 8080!');
+});
