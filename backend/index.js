f637398f2c655408ca08c1f2897a1413e65e8581
Sadhika1012 <111134246+Sadhika1012@users.noreply.github.com>
2023-06-20 10:53:20 +0530

    Update index.js

---
+++ backend/index.js
@@ -0,0 +1,12 @@
+const express  = require('express');
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+const cors = require('cors');
+dotenv.config();
+
+const app = express();
+app.use(cors()); 
+app.use(express.json())
+app.listen(8800, ()=>{
+    console.log('Backend running and server up and running at 8080!');
+})
