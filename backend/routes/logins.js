43445988c6bbdf9526d89f4aa5de3546eadb7c9f
Shreeya Rao <106077939+Shreeyasatish@users.noreply.github.com>
2023-06-21 15:26:19 +0530

    Update logins.js

--- backend/routes/logins.js
+++ backend/routes/logins.js
@@ -3,36 +3,39 @@ const router = express.Router();
 const Login = require('../models/Login');
 
 router.use(express.json());
+
 // Login route
 router.post('/', async (req, res) => {
+  // Request body contains username and password
   const { username, password } = req.body;
-  
-  console.log('Received request:', { username, password});
+
+  console.log('Received request:', { username, password });
   try {
-    const user = await Login.findOne({ username:username });
+    const user = await Login.findOne({ username: username });
 
     if (user) {
       if (password === user.password) {
-        res.status(200).json({ message: 'Login Successful!', user });
-      } else {
-        res.status(401).json({ message: "Password didn't match" });
+        res.status(200).json({ message: 'Login Successful!', success: true });
+      } else if (password !== user.password) {
+        res.status(401).json({ message: "Password didn't match", success: false });
       }
     } else {
-      res.status(404).json({ message: 'User not registered' });
+      res.status(404).json({ message: 'User not registered', success: false });
     }
   } catch (error) {
     console.error(error);
-    res.status(500).json({ error: 'Server error' });
+    res.status(500).json({ message: 'Server error', success: false });
   }
 });
-router.get('/', async (req, res) =>{
-    try{
-        const logins = await Login.find();
-        res.status(200).json(logins);
-    }
-    catch(err){
-        res.status(500).json(err);
-    }
-})
+
+// GET all logins route
+router.get('/', async (req, res) => {
+  try {
+    const logins = await Login.find();
+    res.status(200).json(logins);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
 
 module.exports = router;
