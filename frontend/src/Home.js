3c45a8ecf4df2c6fcea90263be6c99b354ff5849
Sadhika1012 <111134246+Sadhika1012@users.noreply.github.com>
2023-06-22 20:04:30 +0530

    Updated Home.js

--- frontend/src/Home.js
+++ frontend/src/Home.js
@@ -1,28 +1,13 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+
 import './nav.css';
-import add from './img/add-png.png';
+
 import backgroundImage from './img/background10.jpg';
 
 function Home() {
   return (
     <div className="page-container" style={{ backgroundImage: `url(${process.env.PUBLIC_URL + backgroundImage})` }}>
-      <ul className='navbar'>
-        <li className='li1'>
-          <Link to="/buy">Buy products</Link>
-        </li>      
-        <li className='li1'>
-          <Link to="/sell">Sell Products</Link>
-        </li>
-        <li className='li1'>
-          <Link to="/rent">Rent Products</Link>
-        </li>
-        <li>
-          <Link to="/cart">
-            <img className='img1' src={add} alt="cart" />
-          </Link>
-        </li>
-      </ul>
+      
       <div className="white-box">
       <div className="text-content">
         
