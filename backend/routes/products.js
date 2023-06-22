4026e756e73e7a24bf3b876e12544fb81271727a
Shreeya Rao <106077939+Shreeyasatish@users.noreply.github.com>
2023-06-22 21:13:51 +0530

    Add files via upload

--- backend/routes/products.js
+++ backend/routes/products.js
@@ -2,15 +2,35 @@ const express = require('express');
 const router = express.Router();
 const Product = require('../models/Product');
 
-// GET all products
-router.get('/', async (req, res) => {
-    try {
-      const products = await Product.find({});
-      res.send(products);
-    } catch (error) {
-      console.error(error);
-      res.status(500).json({ error: 'Server error' });
-    }
-  });
+router.use(express.json());
 
-  module.exports = router;
+router.post("/", async (req, res) => {
+  const { name,description, price, flag, base64, } = req.body;
+
+  try {
+    await Product.create({
+      name,
+      description,
+      price,
+      flag,
+      image: base64,
+      
+    });
+
+    res.json({ status: "ok" });
+  } catch (error) {
+    res.status(500).json({ status: "error", data: error });
+  }
+});
+
+router.get('/', async (req, res) => {
+  try{
+    await Product.find({}).then(data=>{
+        res.send({status:"ok",data:data})
+    })
+  } catch(error){
+    res.status(500).send({status:"error",data:error});
+  }
+});
+
+module.exports = router;
