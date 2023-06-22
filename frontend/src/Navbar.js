9662bcbab39d63dab756685d555d56de29d1db0e
Sadhika1012 <111134246+Sadhika1012@users.noreply.github.com>
2023-06-22 20:03:42 +0530

    Add files via upload

---
+++ frontend/src/Navbar.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import './nav.css';
+import add from './img/add-png.png';
+
+const Navbar = () => {
+  return (
+    <ul className='navbar'>
+      <li className='li1'>
+        <Link to="/buy">Buy products</Link>
+      </li>      
+      <li className='li1'>
+        <Link to="/sell">Sell Products</Link>
+      </li>
+      <li className='li1'>
+        <Link to="/rent">Rent Products</Link>
+      </li>
+      <li>
+        <Link to="/cart">
+          <img className='img1' src={add} alt="cart" />
+        </Link>
+      </li>
+    </ul>
+  );
+};
+
+export default Navbar;
