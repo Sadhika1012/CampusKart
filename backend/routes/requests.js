972dc0b43b3e757bf6aa88cd807846c7c42d2bc4
Shreeya Rao <106077939+Shreeyasatish@users.noreply.github.com>
2023-06-23 20:11:29 +0530

    Add files via upload

---
+++ backend/routes/requests.js
@@ -0,0 +1,40 @@
+const express = require('express');
+const router = express.Router();
+const Request = require('../models/Request');
+
+// GET /api/requests
+router.get('/', async (req, res) => {
+  try {
+    const requests = await Request.find();
+    res.json(requests);
+  } catch (error) {
+    res.status(500).json({ message: 'Failed to fetch requests' });
+  }
+});
+
+// POST /api/requests
+router.post('/', async (req, res) => {
+  try {
+    const { username, productname, description, requesttype } = req.body;
+    const newRequest = new Request({
+      username,
+      productname,
+      description,
+      requesttype,
+    });
+    await newRequest.save();
+    res.status(201).json({ message: 'Request submitted successfully' });
+  } catch (error) {
+    res.status(500).json({ message: 'Failed to submit request' });
+  }
+});
+router.delete('/:id', async (req, res) => {
+    try {
+      const requestId = req.params.id;
+      await Request.findByIdAndRemove(requestId);
+      res.json({ message: 'Request deleted successfully' });
+    } catch (error) {
+      res.status(500).json({ message: 'Failed to delete request' });
+    }
+  });
+module.exports = router;
