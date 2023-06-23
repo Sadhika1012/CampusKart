e4f0779cd3c2b19d010a5359088cae5f033e59b7
Shreeya Rao <106077939+Shreeyasatish@users.noreply.github.com>
2023-06-23 20:10:58 +0530

    Add files via upload

---
+++ backend/models/Request.js
@@ -0,0 +1,11 @@
+const mongoose = require('mongoose');
+
+const RequestSchema = new mongoose.Schema({
+  username: String,
+  productname:String,
+  description: String,
+  requesttype:String,
+});
+
+const Request = mongoose.model('Request', RequestSchema);
+module.exports = Request;
