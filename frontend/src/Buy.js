c554748e33d69d82c182dcb46fd0b9451cd6525f
Shreeya Rao <106077939+Shreeyasatish@users.noreply.github.com>
2023-06-23 20:08:59 +0530

    Add files via upload

--- frontend/src/Buy.js
+++ frontend/src/Buy.js
@@ -1,10 +1,11 @@
-import React, { useState, useEffect, useContext } from 'react';
-import { CartContext } from './CartContext';
+import React, { useState, useEffect } from 'react';
+import { Link } from 'react-router-dom';
 import './buy.css';
+import './Requestform.js';
 
 const Buy = () => {
-  const { addToCart, cartItems } = useContext(CartContext);
   const [allImage, setAllImage] = useState([]);
+  const [searchTerm, setSearchTerm] = useState('');
 
   useEffect(() => {
     getImage();
@@ -30,55 +31,42 @@ const Buy = () => {
       });
   };
 
-  const handleAddToCart = (item, action) => {
-    addToCart({ item, action });
+  const handleSearch = (e) => {
+    setSearchTerm(e.target.value);
   };
 
-  const isItemInCart = (itemId) => {
-    return cartItems.some((item) => item.item._id === itemId);
-  };
+  const filteredImages = allImage.filter((data) =>
+    data.name.toLowerCase().includes(searchTerm.toLowerCase())
+  );
 
   return (
-    <div className="auth-wrapper" style={{ height: '200vh' }}>
-      {allImage.map((data) => (
-        <div key={data._id} style={{ marginBottom: '20px' }}>
-          <img width={100} height={100} src={data.image} alt="Product" />
-          <p>Name: {data.name}</p>
-          <p>Description: {data.description}</p>
-          <p>Price: {data.price}</p>
-          <div>
-            {data.flag === 0 && (
-              <button
-                onClick={() => handleAddToCart(data, 'buy')}
-                disabled={isItemInCart(data._id)}
-              >
-                Buy
-              </button>
-            )}
-            {data.flag === 1 && (
-              <button
-                onClick={() => handleAddToCart(data, 'rent')}
-                disabled={isItemInCart(data._id)}
-              >
-                Rent
-              </button>
-            )}
-            {data.flag === 2 && (
+    <div>
+      <div className="search-bar">
+        <input type="text" placeholder="Search" onChange={handleSearch} />
+        <Link to="/request" className="request-button">Request</Link>
+      </div>
+      <div className="image-container">
+        <div className="image-grid">
+          {filteredImages.map((data) => (
+            <div key={data._id} className="image-item">
+              <img width={100} height={100} src={data.image} alt="Product" />
+              <p>Name: {data.name}</p>
+              <p>Description: {data.description}</p>
+              <p>Price: {data.price}</p>
               <div>
-                <button onClick={() => handleAddToCart(data)} disabled={isItemInCart(data._id)}>
-                  Buy
-                </button>
-                <button
-                  onClick={() => handleAddToCart(data, 'rent')}
-                  disabled={isItemInCart(data._id)}
-                >
-                  Rent
-                </button>
+                {data.flag === 0 && <button>Buy</button>}
+                {data.flag === 1 && <button>Rent</button>}
+                {data.flag === 2 && (
+                  <div>
+                    <button>Buy</button>
+                    <button>Rent</button>
+                  </div>
+                )}
               </div>
-            )}
-          </div>
+            </div>
+          ))}
         </div>
-      ))}
+      </div>
     </div>
   );
 };
---
+++ frontend/src/Requestform.js
@@ -0,0 +1,95 @@
+import React, { useState } from 'react';
+import './requestform.css';
+
+
+const Requestform = () => {
+  const [username, setUsername] = useState('');
+  const [productName, setProductName] = useState('');
+  const [description, setDescription] = useState('');
+  const [requestType, setRequestType] = useState('');
+ 
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      const response = await fetch('http://localhost:8080/api/requests', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+          username,
+          productname: productName,
+          description,
+          requesttype: requestType,
+        }),
+      });
+      const data = await response.json();
+      console.log(data);
+      // Reset form fields or show success message
+      setUsername('');
+      setProductName('');
+      setDescription('');
+      setRequestType('');
+      alert('Your request has been sent!');
+      window.location.replace('/buy');
+    } catch (error) {
+      console.error('Error submitting request:', error);
+      // Handle error or show error message
+    }
+  };
+
+  return (
+    <div className="request-form">
+      <h2>Request Form</h2>
+      <form onSubmit={handleSubmit} className="request-form">
+        <div className="form-group">
+          <label htmlFor="username">Username</label>
+          <input
+            type="text"
+            id="username"
+            value={username}
+            onChange={(e) => setUsername(e.target.value)}
+          />
+        </div>
+
+        <div className="form-group">
+          <label htmlFor="productName">Product Name</label>
+          <input
+            type="text"
+            id="productName"
+            value={productName}
+            onChange={(e) => setProductName(e.target.value)}
+          />
+        </div>
+
+        <div className="form-group">
+          <label htmlFor="description">Description</label>
+          <textarea
+            id="description"
+            value={description}
+            onChange={(e) => setDescription(e.target.value)}
+          ></textarea>
+        </div>
+
+        <div className="form-group">
+          <label htmlFor="requestType">Request Type</label>
+          <select
+            id="requestType"
+            value={requestType}
+            onChange={(e) => setRequestType(e.target.value)}
+          >
+            <option value="">Select</option>
+            <option value="buy">Buy</option>
+            <option value="rent">Rent</option>
+          </select>
+        </div>
+
+        <button type="submit">Submit</button>
+        
+      </form>
+    </div>
+  );
+};
+
+export default Requestform;
