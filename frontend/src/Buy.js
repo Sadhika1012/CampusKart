ce8a21e12326e73b211c1e807703626452be0077
Sadhika1012 <111134246+Sadhika1012@users.noreply.github.com>
2023-06-23 19:43:18 +0530

    Updated Buy.js

--- frontend/src/Buy.js
+++ frontend/src/Buy.js
@@ -1,6 +1,9 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useContext } from 'react';
+import { CartContext } from './CartContext';
+import './buy.css';
 
 const Buy = () => {
+  const { addToCart, cartItems } = useContext(CartContext);
   const [allImage, setAllImage] = useState([]);
 
   useEffect(() => {
@@ -27,21 +30,50 @@ const Buy = () => {
       });
   };
 
+  const handleAddToCart = (item, action) => {
+    addToCart({ item, action });
+  };
+
+  const isItemInCart = (itemId) => {
+    return cartItems.some((item) => item.item._id === itemId);
+  };
+
   return (
-    <div>
+    <div className="auth-wrapper" style={{ height: '200vh' }}>
       {allImage.map((data) => (
-        <div key={data._id}>
+        <div key={data._id} style={{ marginBottom: '20px' }}>
           <img width={100} height={100} src={data.image} alt="Product" />
           <p>Name: {data.name}</p>
           <p>Description: {data.description}</p>
           <p>Price: {data.price}</p>
           <div>
-            {data.flag === 0 && <button>Buy</button>}
-            {data.flag === 1 && <button>Rent</button>}
+            {data.flag === 0 && (
+              <button
+                onClick={() => handleAddToCart(data, 'buy')}
+                disabled={isItemInCart(data._id)}
+              >
+                Buy
+              </button>
+            )}
+            {data.flag === 1 && (
+              <button
+                onClick={() => handleAddToCart(data, 'rent')}
+                disabled={isItemInCart(data._id)}
+              >
+                Rent
+              </button>
+            )}
             {data.flag === 2 && (
               <div>
-                <button>Buy</button>
-                <button>Rent</button>
+                <button onClick={() => handleAddToCart(data)} disabled={isItemInCart(data._id)}>
+                  Buy
+                </button>
+                <button
+                  onClick={() => handleAddToCart(data, 'rent')}
+                  disabled={isItemInCart(data._id)}
+                >
+                  Rent
+                </button>
               </div>
             )}
           </div>
