3b5ee11684c2be906ca4bb0317d2c4bc7fc8c4a8
Sadhika1012 <111134246+Sadhika1012@users.noreply.github.com>
2023-06-23 22:17:08 +0530

    Updated Buy.js with handleAddToCart

--- frontend/src/Buy.js
+++ frontend/src/Buy.js
@@ -1,12 +1,13 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useContext } from 'react';
+import { CartContext } from './CartContext';
 import { Link } from 'react-router-dom';
 import './buy.css';
 import './Requestform.js';
 
 const Buy = () => {
+  const { addToCart, cartItems } = useContext(CartContext);
   const [allImage, setAllImage] = useState([]);
   const [searchTerm, setSearchTerm] = useState('');
-
   useEffect(() => {
     getImage();
   }, []);
@@ -30,7 +31,6 @@ const Buy = () => {
         console.error("Error fetching images:", error);
       });
   };
-
   const handleSearch = (e) => {
     setSearchTerm(e.target.value);
   };
@@ -39,34 +39,51 @@ const Buy = () => {
     data.name.toLowerCase().includes(searchTerm.toLowerCase())
   );
 
+  const handleAddToCart = (item, action) => {
+    addToCart({ item, action });
+  };
+
+  const isItemInCart = (itemId) => {
+    return cartItems.some((item) => item.item._id === itemId);
+  };
+
   return (
     <div>
       <div className="search-bar">
         <input type="text" placeholder="Search" onChange={handleSearch} />
-        <Link to="/request" className="request-button">Request</Link>
+        <Link to="/requestform" className="request-button">Request</Link>
       </div>
       <div className="image-container">
         <div className="image-grid">
-          {filteredImages.map((data) => (
-            <div key={data._id} className="image-item">
-              <img width={100} height={100} src={data.image} alt="Product" />
-              <p>Name: {data.name}</p>
-              <p>Description: {data.description}</p>
-              <p>Price: {data.price}</p>
-              <div>
-                {data.flag === 0 && <button>Buy</button>}
-                {data.flag === 1 && <button>Rent</button>}
-                {data.flag === 2 && (
-                  <div>
-                    <button>Buy</button>
-                    <button>Rent</button>
-                  </div>
-                )}
-              </div>
-            </div>
-          ))}
+      {filteredImages.map((data) => (
+        <div key={data._id} className="image-item">
+          <img width={100} height={100} src={data.image} alt="Product" />
+          <p>Name: {data.name}</p>
+          <p>Description: {data.description}</p>
+          <p>Price: {data.price}</p>
+          <div>
+            {data.flag === 0 && (
+              <button
+                onClick={() => handleAddToCart(data, 'buy')}
+                disabled={isItemInCart(data._id)}
+              >
+                Buy
+              </button>
+            )}
+            {data.flag === 1 && (
+              <button
+                onClick={() => handleAddToCart(data, 'rent')}
+                disabled={isItemInCart(data._id)}
+              >
+                Rent
+              </button>
+            )}
+           
+          </div>
         </div>
-      </div>
+      ))}
+    </div>
+    </div>
     </div>
   );
 };
