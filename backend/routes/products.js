c22eddd824e1c00909c95609a38dbd59d3dccdb3
Sadhika1012 <111134246+Sadhika1012@users.noreply.github.com>
2023-06-23 17:36:56 +0530

    Update products.js

--- backend/routes/products.js
+++ backend/routes/products.js
@@ -4,24 +4,7 @@ const Product = require('../models/Product');
 
 router.use(express.json());
 
-router.post("/", async (req, res) => {
-  const { name,description, price, flag, base64, } = req.body;
 
-  try {
-    await Product.create({
-      name,
-      description,
-      price,
-      flag,
-      image: base64,
-      
-    });
-
-    res.json({ status: "ok" });
-  } catch (error) {
-    res.status(500).json({ status: "error", data: error });
-  }
-});
 
 router.get('/', async (req, res) => {
   try{
