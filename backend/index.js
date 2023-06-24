17a5d51d523f52b3c9d4f631214cbfd21af4c21e
Sadhika1012 <111134246+Sadhika1012@users.noreply.github.com>
2023-06-24 23:23:12 +0530

    Updated index.js with socket.io

--- backend/index.js
+++ backend/index.js
@@ -8,12 +8,21 @@ const sellsRouter=require('./routes/sells');
 const productsRouter = require('./routes/products');
 const bodyParser = require('body-parser');
 const requestsRouter = require('./routes/requests');
+const { Server } = require("socket.io");
+const http = require('http');
 dotenv.config();
 
 const app = express();
 app.use(cors());
 app.use(bodyParser.json({ limit: '10mb' }));
 app.use(express.json());
+const server = http.createServer(app);
+const io = new Server(server, {
+  cors: {
+    origin: "http://localhost:3000",
+    methods: ["GET", "POST"],
+  },
+});
 
 mongoose.connect("mongodb://localhost:27017/CampusKart", {
     useNewUrlParser: true,
@@ -24,11 +33,35 @@ mongoose.connect("mongodb://localhost:27017/CampusKart", {
   })
   .catch((err) => console.log(err));
 
+const userUserRoom = 1;
+  const userSellerRoom = 2;
+  
+  io.on("connection", (socket) => {
+    console.log(`User Connected: ${socket.id}`);
+  
+    socket.on("join_room", (data) => {
+      if (data === userUserRoom || data === userSellerRoom) {
+        socket.join(data);
+        console.log(`User with ID: ${socket.id} joined room: ${data}`);
+      } else {
+        console.log(`User with ID: ${socket.id} requested an invalid room: ${data}`);
+      }
+    });
+  
+    socket.on("send_message", (data) => {
+      socket.to(data.room).emit("receive_message", data);
+    });
+  
+    socket.on("disconnect", () => {
+      console.log("User Disconnected", socket.id);
+    });
+  });
+
 app.use('/api/logins', loginsRouter);
 app.use('/api/products', productsRouter);
 app.use('/api/orders',ordersRouter);
 app.use('/api/requests',requestsRouter);
 app.use('/api/sells',sellsRouter);
-app.listen(8080, () => {
+server.listen(8080, () => {
   console.log('Backend running and server up and running at 8080!');
 });
