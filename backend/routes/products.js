8e0b5f10e8c4815f52be66f2dd17b7c85500928a
Shreya <109687884+Shreya-R-2003@users.noreply.github.com>
2023-06-25 00:01:28 +0530

    Update products.js

--- backend/routes/products.js
+++ backend/routes/products.js
@@ -4,15 +4,25 @@ const Product = require('../models/Product');
 
 router.use(express.json());
 
-
-
 router.get('/', async (req, res) => {
-  try{
-    await Product.find({}).then(data=>{
-        res.send({status:"ok",data:data})
-    })
-  } catch(error){
-    res.status(500).send({status:"error",data:error});
+  try {
+    const { category } = req.query; // Get the category filter from query parameters
+
+    let filter = {}; // Create an empty filter object
+
+    if (category && category !== 'all') {
+      filter.category = category; // Add the category filter to the filter object if it's not 'all'
+    }
+
+    await Product.find(filter)
+      .then((data) => {
+        res.send({ status: 'ok', data: data });
+      })
+      .catch((error) => {
+        res.status(500).send({ status: 'error', data: error });
+      });
+  } catch (error) {
+    res.status(500).send({ status: 'error', data: error });
   }
 });
 
