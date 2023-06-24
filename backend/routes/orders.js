757104c36e946ffb705425e1077de47bb6fb817a
Shreya <109687884+Shreya-R-2003@users.noreply.github.com>
2023-06-24 19:18:00 +0530

    Update orders.js

--- backend/routes/orders.js
+++ backend/routes/orders.js
@@ -18,20 +18,25 @@ router.post("/", async (req, res) => {
   
 });
 
-router.get("/", async (req, res) => {
-  const loggedInUsername = req.query.username;
+// API Endpoint: /api/orders/:username
+router.get("/:username", async (req, res) => {
+  const loggedInUsername = req.params.username;
 
   if (!loggedInUsername) {
     return res.status(400).json({ error: 'No username provided.' });
   }
 
   Order.find({ username: loggedInUsername })
+    // .sort({ createdAt: -1 }) // Sort by creation timestamp in descending order
+    // .limit(1) // Limit the result to 1 order (the most recent)
     .then((orders) => {
-      res.status(200).json({ orders: orders });
+      console.log(orders)
+      res.status(200).json({ order: orders }); // Send the most recent order
     })
     .catch((error) => {
-      res.status(500).json({ error: 'An error occurred while fetching the orders.' });
+      res.status(500).json({ error: 'An error occurred while fetching the order.' });
     });
 });
+
 
 module.exports = router;
