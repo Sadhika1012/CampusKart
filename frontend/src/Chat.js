fe08f8a6a3140bad17fe16fcb8ef11c6d7d76e55
Sadhika1012 <111134246+Sadhika1012@users.noreply.github.com>
2023-06-25 19:07:34 +0530

    Updated Chat.js

--- frontend/src/Chat.js
+++ frontend/src/Chat.js
@@ -1,13 +1,13 @@
-import './Chat.css'
+import "./Chat.css";
 import io from "socket.io-client";
-import { useState, useEffect } from "react";
+import { useState,useEffect } from "react";
 import ChatForm from "./ChatForm";
 
 const socket = io.connect("http://localhost:8080");
 
 function Chat() {
   const storedUsername = localStorage.getItem('username');
-  const [username, setUsername] = useState(storedUsername || "");
+  const [username, setUsername] = useState("");
   const [room, setRoom] = useState("");
   const [showChat, setShowChat] = useState(false);
 
@@ -19,24 +19,11 @@ function Chat() {
 
   const joinRoom = () => {
     if (username !== "" && room !== "") {
-      if (room === "1") {
-        socket.emit("join_room", room);
-        setShowChat(true);
-      } else if (room === "2") {
-        socket.emit("join_room", room);
-        setShowChat(true);
-        // Display "How can I help you?" message
-        socket.emit("send_message", {
-          room: room,
-          message: "How can I help you?",
-          sender: "System"
-        });
-      } else {
-        alert("Invalid room. Please choose one of the specified rooms.");
-      }
+      socket.emit("join_room", room);
+      setShowChat(true);
     }
   };
-  
+
   return (
     <div className="Chat">
       {!showChat ? (
@@ -44,7 +31,7 @@ function Chat() {
           <h3>Join A Chat</h3>
           <input
             type="text"
-            placeholder="Name."
+            placeholder="User"
             value={username}
             onChange={(event) => {
               setUsername(event.target.value);
@@ -57,19 +44,13 @@ function Chat() {
               setRoom(event.target.value);
             }}
           />
-          <div className="roomOptions">
-            <p>Room Options:</p>
-            <ol>
-              <li onClick={() => setRoom("general")}>General Chat</li>
-              <li onClick={() => setRoom("contact-seller")}>Contact a Seller</li>
-            </ol>
-          </div>
           <button onClick={joinRoom}>Join A Room</button>
         </div>
       ) : (
         <ChatForm socket={socket} username={username} room={room} />
       )}
     </div>
-  );}
-  
-  export default Chat;
+  );
+}
+
+export default Chat;
